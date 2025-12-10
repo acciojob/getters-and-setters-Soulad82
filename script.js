a553cb19f2abbab93222b2603bde0e51d07fb63a
Adwait Warkad <58514858+Soulad82@users.noreply.github.com>
2025-12-10 12:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name: string, age:number){
+		this.name = name;
+		this.age = age;
+	}
+	get name(name){
+		this.name=name;
+	}
+	set age(age:numbeer){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	public static void study(){
+		console.log(name," is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	public static void teach(){
+		console.log(name," is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
